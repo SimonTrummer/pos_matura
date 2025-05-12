77f94b9ea6acf5342bb80adabf07a7c00372dcd9
SimonTrummer <simon.trum@gmail.com>
2025-05-12 11:41:32 +0200

    readTree

--- src/main/java/at/kaindorf/matura_lernen2/db/InnitData.java
+++ src/main/java/at/kaindorf/matura_lernen2/db/InnitData.java
@@ -1,9 +1,8 @@
 package at.kaindorf.matura_lernen2.db;
 
-import at.kaindorf.matura_lernen2.pojos.Account;
-import at.kaindorf.matura_lernen2.pojos.Customer;
-import at.kaindorf.matura_lernen2.pojos.SavingsAccount;
+import at.kaindorf.matura_lernen2.pojos.*;
 import at.kaindorf.matura_lernen2.repositories.CustomerRepository;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +10,10 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -24,8 +26,14 @@ import java.util.stream.Collectors;
 public class InnitData implements ApplicationRunner {
     private final CustomerRepository customerRepository;
 
+    public static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
+       jsonReadTree();
+    }
+
+    public void jsonObjectMapper() throws IOException {
         InputStream is = getClass().getResourceAsStream("/bankAccount_customer.json");
         ObjectMapper om = new ObjectMapper().registerModule(new JavaTimeModule());
 
@@ -48,6 +56,33 @@ public class InnitData implements ApplicationRunner {
         });
 
         customerRepository.saveAll(customers);
+    }
 
+    public void jsonReadTree() throws IOException {
+        InputStream is = getClass().getResourceAsStream("/bankAccount_customer.json");
+        ObjectMapper om = new ObjectMapper().registerModule(new JavaTimeModule());
+
+        JsonNode rootNode = om.readTree(is);
+        if (!rootNode.isArray()) {
+            return;
+        }
+
+        for (JsonNode node: rootNode) {
+            String firstname = node.get("firstname").asText();
+            String lastname = node.get("lastname").asText();
+            Integer customerNumber = node.get("customerNumber").asInt();
+            LocalDate birthdate = LocalDate.parse(node.get("birthdate").asText(),dtf);
+            Gender gender = Gender.valueOf(node.get("gender").asText());
+            JsonNode addressRootNode = node.get("address");
+            String streetname = addressRootNode.get("streetname").asText();
+            String city = addressRootNode.get("city").asText();
+            Integer streetnumber = addressRootNode.get("streetNumber").asInt();
+            Integer zipCode = addressRootNode.get("zipCode").asInt();
+
+            Address address = new Address(null,streetname,streetnumber,zipCode,city,null);
+            Customer customer = new Customer(null,firstname,lastname,customerNumber,birthdate, gender, new ArrayList<>(),address);
+            address.setCustomer(customer);
+            customerRepository.save(customer);
+        }
     }
 }
